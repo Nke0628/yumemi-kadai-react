31125ebf01a82f721e11936d2d2fe5e93bc3ae47
Nke0628 <ooo_diva@yahoo.co.jp>
2023-01-15 18:16:23 +0900

    YKR-6 APIKEYをコンテキストで管理する

--- yumemi/src/components/pages/Contents.tsx
+++ yumemi/src/components/pages/Contents.tsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 export const Contents: React.FC = () => {
-  return <div>Contents</div>
+  //TODO 都道府県取得処理
+  const { authApiKey, setAuthApiKey } = useContext(AuthContext)
+  useEffect(() => {
+    const axiosInstance = axios.create({
+      baseURL: 'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+      headers: {
+        'X-API-KEY': authApiKey,
+      },
+    })
+    axiosInstance.get('').then((res) => {
+      alert(res)
+    })
+  }, [])
+  return <div>Contents{authApiKey}</div>
 }
--- yumemi/src/main.tsx
+++ yumemi/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouteConfig } from './routes/RouteConfig'
 import './index.css'
 import { Container } from './components/layout/Container'
+import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Container>
-      <RouteConfig />
-    </Container>
+    <AuthProvider>
+      <Container>
+        <RouteConfig />
+      </Container>
+    </AuthProvider>
   </React.StrictMode>,
 )
